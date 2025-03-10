e020a9d4f15dbf18c594338ada46db213bc45822
ocavue <ocavue@gmail.com>
2025-03-10 13:01:56 +1100

    docs: update demo website

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,30 +1,55 @@
 // @ts-check
-import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
 import starlightThemeNova from 'starlight-theme-nova'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      plugins: [starlightThemeNova()],
+
+      title: 'Demo Docs',
       social: {
         github: 'https://github.com/withastro/starlight',
       },
+
+      // Sidebar — mostly dummy links just to demonstrate some structure
       sidebar: [
+        { slug: 'splash', label: 'Home' },
         {
           label: 'Guides',
           items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', slug: 'guides/example' },
+            { label: 'Example Guide', slug: 'index' },
+            { label: 'Installation', link: '#' },
+            { label: 'Testing', link: '#', badge: 'New' },
           ],
         },
         {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: 'Constellations',
+          items: [
+            { label: 'Carina', link: '#' },
+            { label: 'Centaurus', link: '#' },
+            {
+              label: 'Seasonal',
+              items: [
+                { label: 'Andromeda', link: '#' },
+                { label: 'Orion', link: '#' },
+                { label: 'Ursa Minor', link: '#' },
+              ],
+            },
+          ],
+        },
+        { label: 'Playground', link: '#' },
+        {
+          label: 'Blog',
+          link: '#',
+          badge: { text: 'WIP', variant: 'caution' },
         },
       ],
-      plugins: [starlightThemeNova()],
     }),
   ],
+
+  // Toolbar disabled so it doesn’t appear in screenshots
+  devToolbar: { enabled: false },
 })
