85a42077487c4cbaed30ae2957dceadb26a367b4
ocavue <ocavue@gmail.com>
2025-03-10 16:46:02 +1100

    feat: add code-tabs

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -40,7 +40,10 @@ export default defineConfig({
             },
           ],
         },
-        { label: 'Playground', link: '#' },
+        {
+          label: 'Components',
+          autogenerate: { directory: 'components' },
+        },
         {
           label: 'Blog',
           link: '#',
