dc83307afbece66b4e18c83b3bf4e8980b4c90b4
ocavue <ocavue@users.noreply.github.com>
2025-04-05 15:15:17 +1100

    feat: add override support for more components (#23)

--- packages/starlight-theme-nova/src/virtual-internal.d.ts
+++ packages/starlight-theme-nova/src/virtual-internal.d.ts
@@ -4,3 +4,19 @@ declare module 'virtual:starlight-theme-nova/user-config' {
   const options: import('./user-options').ThemeNovaOptions
   export default options
 }
+declare module 'virtual:starlight/components/Search' {
+  const Search: typeof import('./components/Search.astro').default
+  export default Search
+}
+declare module 'virtual:starlight/components/SiteTitle' {
+  const SiteTitle: typeof import('./components/SiteTitle.astro').default
+  export default SiteTitle
+}
+declare module 'virtual:starlight/components/SocialIcons' {
+  const SocialIcons: typeof import('./components/SocialIcons.astro').default
+  export default SocialIcons
+}
+declare module 'virtual:starlight/components/ThemeSelect' {
+  const ThemeSelect: typeof import('./components/ThemeSelect.astro').default
+  export default ThemeSelect
+}
