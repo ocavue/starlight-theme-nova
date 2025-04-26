ab449f36ff16414b6279e05de5020dfb4898889b
ocavue <ocavue@gmail.com>
2025-04-26 13:42:53 +1000

    feat: update twoslash renderer colors

--- packages/shiki-twoslash-renderer/tsdown.config.ts
+++ packages/shiki-twoslash-renderer/tsdown.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'tsdown'
+import LightningCSS from 'unplugin-lightningcss/rolldown'
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/elements.ts', 'src/style.css'],
   format: 'esm',
   dts: true,
+  plugins: [
+    LightningCSS({
+      options: {
+        targets: {
+          chrome: 100,
+        },
+      },
+    }),
+  ],
 })
