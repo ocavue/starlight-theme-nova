d9c468cf759d1e7fa64d47caf8648f67c2a7d744
ocavue <ocavue@gmail.com>
2025-06-12 12:58:06 +1000

    fix typecheck

--- packages/starlight-theme-nova/src/types.d.ts
+++ packages/starlight-theme-nova/src/types.d.ts
@@ -1,2 +1,16 @@
 import 'astro/client'
 import '@astrojs/starlight'
+
+declare module 'astro:content' {
+  export interface RenderResult {
+    Content: import('astro/runtime/server/index.js').AstroComponentFactory
+  }
+}
+
+type ContentConfig = typeof import('./content.config.js')
+
+type ReturnTypeOrOriginal<T> = T extends (...args: any[]) => infer R ? R : T
+
+export type StarlightRouteEntryData = import('astro/zod').infer<
+  ReturnTypeOrOriginal<Required<ContentConfig['collections']['docs']>['schema']>
+>
