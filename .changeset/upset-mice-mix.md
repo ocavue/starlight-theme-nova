935e8ffbf452359b900909bb31c11bfc45e21cf3
ocavue <ocavue@gmail.com>
2025-08-06 01:53:50 +1000

    fix: provide better i18n error message

---
+++ .changeset/upset-mice-mix.md
@@ -0,0 +1,5 @@
+---
+'starlight-theme-nova': patch
+---
+
+Provide a better error message when a translation is not found.
--- packages/starlight-theme-nova/src/user-options.ts
+++ packages/starlight-theme-nova/src/user-options.ts
@@ -1,5 +1,13 @@
 export interface NavItem {
+  /**
+   * The visible label for this item in the navigation. It could be a string or
+   * a record of strings with the language code as the key.
+   */
   label: string | Record<string, string>
+  /**
+   * The link to this item’s content. It could be a string or a record of
+   * strings with the language code as the key.
+   */
   href: string | Record<string, string>
 }
 
