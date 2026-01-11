a4754e768ed1e1c57c71db406a6b995a4838abaa
ocavue <ocavue@users.noreply.github.com>
2026-01-11 18:45:03 +1100

    refactor: inline tailwindcss class names (#129)

--- packages/starlight-theme-nova/src/constants.ts
+++ packages/starlight-theme-nova/src/constants.ts
@@ -1,5 +1,7 @@
 export const PAGE_TITLE_ID = '_top'
 
+// data-nova-code-container
 export const CODE_PROPERTY_CONTAINER = 'dataNovaCodeContainer'
 
+// data-nova-code-title
 export const CODE_PROPERTY_TITLE = 'dataNovaCodeTitle'
