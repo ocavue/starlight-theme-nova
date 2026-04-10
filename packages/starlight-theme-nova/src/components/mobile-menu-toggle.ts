358ff180e81759158bc7397a5bf1e27c39077fe6
ocavue <ocavue@gmail.com>
2026-04-10 17:33:09 +1000

    w

--- packages/starlight-theme-nova/src/components/mobile-menu-toggle.ts
+++ packages/starlight-theme-nova/src/components/mobile-menu-toggle.ts
@@ -1,28 +1,20 @@
-import type { BaseElement } from '@aria-ui/core'
+import type { HostElement } from '@aria-ui/core'
 import {
   createSignal,
   defineCustomElement,
-  useAriaAttribute,
   useEffect,
   useEventListener,
 } from '@aria-ui/core'
 
-function setupMobileMenuToggle(host: BaseElement) {
-  const expanded = createSignal(false)
+function setupMobileMenuToggle(host: HostElement) {
+  const { get: getExpanded, set: setExpanded } = createSignal(false)
 
-  function toggleExpanded() {
-    expanded.set(!expanded.get())
-  }
-
-  useAriaAttribute(host, 'aria-expanded', () => {
-    return expanded.get() ? 'true' : 'false'
-  })
+  const toggleExpanded = () => setExpanded(!getExpanded())
 
   useEffect(host, () => {
-    document.body.toggleAttribute(
-      'data-mobile-menu-expanded',
-      expanded.get() || false,
-    )
+    const expanded: boolean  = getExpanded()
+    host.setAttribute('aria-controls', expanded ? 'true' : 'false')
+    document.body.toggleAttribute('data-mobile-menu-expanded', expanded || false )
   })
 
   useEventListener(host, 'click', toggleExpanded)
@@ -38,7 +30,7 @@ function setupMobileMenuToggle(host: BaseElement) {
     if (!sidebarNav) return
     const handleKeyup = (event: KeyboardEvent) => {
       if (event.code === 'Escape') {
-        expanded.set(false)
+        setExpanded(false)
         host.focus()
       }
     }
@@ -52,11 +44,7 @@ function setupMobileMenuToggle(host: BaseElement) {
   })
 }
 
-const MobileMenuToggle = defineCustomElement({
-  props: {},
-  events: {},
-  setup: setupMobileMenuToggle,
-})
+const MobileMenuToggle = defineCustomElement(setupMobileMenuToggle, {})
 
 function register() {
   customElements.define('nova-mobile-menu-toggle', MobileMenuToggle)
