74769f80fbbb5766a1b0b659bdc75335709247b9
ocavue <ocavue@gmail.com>
2026-09-04 00:26:14 +1000

    fix: declare `virtual:starlight/user-config` types

--- packages/starlight-theme-nova/src/virtual-internal.d.ts
+++ packages/starlight-theme-nova/src/virtual-internal.d.ts
@@ -1,3 +1,8 @@
+declare module 'virtual:starlight/user-config' {
+  const config: import('@astrojs/starlight/types').StarlightConfig
+  export default config
+}
+
 // Copied from https://github.com/withastro/starlight/blob/9d3ba179c5d524c1c61d771ceb1a7b4e754bee16/packages/starlight/virtual-internal.d.ts#L25
 declare module 'virtual:starlight/pagefind-config' {
   import type { StarlightConfig } from '@astrojs/starlight/types'
